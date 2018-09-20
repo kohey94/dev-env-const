96e93e4bdd75db6fa40f3b80f52145c4d2bb8350
KoHey94_WSL18 <sugawara622@gmail.com>
2018-09-20 11:18:53 +0900

    add nodejs script

---
+++ install_node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#node.jsをn packageでインストールする
+
+sudo apt install -y nodejs npm
+sudo npm cache clean
+sudo npm install n -g
+sudo n stable
+sudo ln -sf /usr/local/bin/node /usr/bin/node
+
